501eab5142d44cd984895af03f2323bb42fb65d8
phil-moura <phil.moura@dhsc.gov.uk>
2023-05-16 07:08:55 +0000

    Changes made during opensafely tutorial age histograms

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -7,7 +7,21 @@ study = StudyDefinition(
         "rate": "uniform",
         "incidence": 0.5,
     },
+
+    # the line below means I am interested with patients who
+    # haven't changed practices between these two dates
     population=patients.registered_with_one_practice_between(
         "2019-02-01", "2020-02-01"
     ),
+
+    # give the age of each patient on a given data and 
+    # I expect every patient to have an age that matches 
+    # the UK population
+    age=patients.age_as_of(
+        "2019-09-01",
+        return_expectations={
+            "rate": "universal",
+            "int": {"distribution": "population_ages"},
+        }
+    )
 )
